b3c784c8112e7e5da4b390b30115f4dc671b66e4
pyrareae <pyrareae@gmail.com>
2022-01-24 20:04:09 -0800

    update emacs config

---
+++ personal/custom.el
@@ -0,0 +1,50 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(cua-mode t nil (cua-base))
+ '(custom-safe-themes
+   '("69b30fcd01e0bce8accefc2fd2f241b84ecbec13ec49719cdda5df550073886e" "0feb7052df6cfc1733c1087d3876c26c66410e5f1337b039be44cb406b6187c6" "27a1dd6378f3782a593cc83e108a35c2b93e5ecc3bd9057313e1d88462701fcd" "f703efe04a108fcd4ad104e045b391c706035bce0314a30d72fbf0840b355c2c" "5a04c3d580e08f5fc8b3ead2ed66e2f0e5d93643542eec414f0836b971806ba9" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" default))
+ '(electric-indent-mode nil)
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(package-selected-packages
+   '(polymode helm-rg helm-sly helm-ag helm-descbinds helm-projectile buffer-flip zprint-format projectile-ripgrep horizon-theme moe-theme helm sass-mode slim-mode sly-quicklisp sly quickrun vterm enh-ruby-mode robe idea-darkula-theme multiple-cursors adaptive-wrap pug-mode treemacs-magit treemacs-projectile vdiff-magit treemacs magit-delta yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window))
+ '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   '((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3")))
+ '(vc-annotate-very-old-color "#DC8CC3")
+ '(word-wrap t)
+ '(xterm-mouse-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ personal/lunarconfig.el
@@ -0,0 +1,146 @@
+(load "server")
+(unless (server-running-p) (server-start))
+(require 'prelude-helm-everywhere)
+
+;; Vars
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+(setq js-indent-level 2)
+(setq indent-line-function 'insert-tab)
+(setq split-height-threshold nil)
+(setq create-lockfiles nil)
+
+;; Global modes
+(global-display-line-numbers-mode t)
+(smartparens-global-mode t)
+
+;; Mode hooks
+;; File associations
+
+;(add-to-list 'auto-mode-alist '("\\.vue\\'" .  vue-mode ))
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(add-hook 'treemacs-mode-hook (lambda() (display-line-numbers-mode -1)))
+
+;(add-hook 'vue-mode-hook #'lsp)
+(add-hook 'ruby-mode-hook 'robe-mode)
+
+(add-hook 'text-mode-hook #'adaptive-wrap-prefix-mode)
+;(add-hook 'vue-mode-hook (lambda () (setq syntax-ppss-table nil)))
+
+;; other hooks
+(add-hook 'mmm-major-mode-hook
+          (lambda ()
+            (set-face-background 'mmm-default-submode-face nil)))
+
+
+;; Bindings
+(global-set-key (kbd "C-s-<up>") 'move-text-up)
+(global-set-key (kbd "C-s-<down>") 'move-text-down)
+(global-set-key (kbd "C-x M-t") 'treemacs)
+(global-unset-key (kbd "M-<down-mouse-1>"))
+(global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "s-SPC") 'projectile-find-file)
+;; (global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(define-key magit-file-section-map (kbd "RET") 'magit-diff-visit-file-other-window)
+
+
+;; MISC
+
+;; transparent terminal bg
+(defun on-frame-open (&optional frame)
+  "If the FRAME created in terminal don't load background color."
+  (unless (display-graphic-p frame)
+    (set-face-background 'default "unspecified-bg" frame)))
+
+(add-hook 'after-make-frame-functions 'on-frame-open)
+
+(require 'robe)
+
+(windmove-default-keybindings)
+
+;; buffer tab conf
+;; key to begin cycling buffers.  Global key.
+(global-set-key (kbd "C-<tab>") 'buffer-flip)
+
+;; transient keymap used once cycling starts
+(setq buffer-flip-map
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "C-<tab>")   'buffer-flip-forward)
+        (define-key map (kbd "C-S-<tab>") 'buffer-flip-backward)
+        (define-key map (kbd "C-ESC")     'buffer-flip-abort)
+        map))
+
+;; buffers matching these patterns will be skipped
+(setq buffer-flip-skip-patterns
+      '("^\\*helm\\b"
+        "^\\*swiper\\*$"))
+
+
+;; VUE setup
+;; source https://gist.github.com/SjB/07cdce0f1fba171704d93c2989077f4d
+
+
+(use-package polymode
+        :ensure t
+        :defer t
+        :hook (vue-mode . lsp-deferred)
+        :mode ("\\.vue\\'" . vue-mode)
+        :config
+        (define-innermode poly-vue-template-innermode
+          :mode 'html-mode
+          :head-matcher "^<[[:space:]]*\\(?:template\\)[[:space:]]*>"
+          :tail-matcher "^</[[:space:]]*\\(?:template\\)[[:space:]]*>"
+          :head-mode 'host
+          :tail-mode 'host)
+
+        (define-innermode poly-vue-script-innermode
+          :mode 'js-mode
+          :head-matcher "<[[:space:]]*\\(?:script\\)[[:space:]]*>"
+          :tail-matcher "</[[:space:]]*\\(?:script\\)[[:space:]]*>"
+          :head-mode 'host
+          :tail-mode 'host)
+
+        (define-auto-innermode poly-vue-template-tag-lang-innermode
+          :head-matcher "^<[[:space:]]*\\(?:template\\)[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*[[:alpha:]]+[[:space:]]*[\"'][[:space:]]*>"
+          :tail-matcher "^</[[:space:]]*\\(?:template\\)[[:space:]]*>"
+          :mode-matcher (cons  "^<[[:space:]]*\\(?:template\\)[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*\\([[:alpha:]]+\\)[[:space:]]*[\"'][[:space:]]*>" 1)
+          :head-mode 'host
+          :tail-mode 'host)
+
+        (define-auto-innermode poly-vue-script-tag-lang-innermode
+          :head-matcher "<[[:space:]]*\\(?:script\\)[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*[[:alpha:]]+[[:space:]]*[\"'][[:space:]]*>"
+          :tail-matcher "</[[:space:]]*\\(?:script\\)[[:space:]]*>"
+          :mode-matcher (cons  "<[[:space:]]*\\(?:script\\)[[:space:]]*lang=[[:space:]]*[\"'][[:space:]]*\\([[:alpha:]]+\\)[[:space:]]*[\"'][[:space:]]*>" 1)
+          :head-mode 'host
+          :tail-mode 'host)
+
+        (define-auto-innermode poly-vue-style-tag-lang-innermode
+          :head-matcher "<[[:space:]]*\\(?:style\\)\\(?:scoped\\|[[:space:]]\\)*lang=[[:space:]]*[\"'][[:space:]]*[[:alpha:]]+[[:space:]]*[\"']*\\(?:scoped\\|[[:space:]]\\)*>"
+          :tail-matcher "</[[:space:]]*\\(?:style\\)[[:space:]]*>"
+          :mode-matcher (cons  "<[[:space:]]*\\(?:style\\)\\(?:scoped\\|[[:space:]]\\)*lang=[[:space:]]*[\"'][[:space:]]*\\([[:alpha:]]+\\)[[:space:]]*[\"']\\(?:scoped\\|[[:space:]]\\)*>" 1)
+          :head-mode 'host
+          :tail-mode 'host)
+
+        (define-innermode poly-vue-style-innermode
+          :mode 'css-mode
+          :head-matcher "<[[:space:]]*\\(?:style\\)[[:space:]]*\\(?:scoped\\|[[:space:]]\\)*>"
+          :tail-matcher "</[[:space:]]*\\(?:style\\)[[:space:]]*>"
+          :head-mode 'host
+          :tail-mode 'host)
+
+        (define-polymode vue-mode
+          :hostmode 'poly-sgml-hostmode
+          :innermodes '(
+                        poly-vue-template-tag-lang-innermode
+                        poly-vue-script-tag-lang-innermode
+                        poly-vue-style-tag-lang-innermode
+                        poly-vue-template-innermode
+                        poly-vue-script-innermode
+                        poly-vue-style-innermode
+                        )))
